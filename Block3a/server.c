3879861fb25f61bb9ec8bb7a6e3d3b42f57ff678
basselfa <38949665+basselfa@users.noreply.github.com>
2018-11-12 12:25:34 +0100

    Add files via upload

--- Block3a/server.c
+++ Block3a/server.c
@@ -7,11 +7,11 @@
 #include <limits.h>
 #include <setjmp.h>
 #include <unistd.h>
-#include <sys/socket.h>		
-#include <netinet/in.h>		
+#include <sys/socket.h>   
+#include <netinet/in.h>   
 #include <poll.h>
-#include <netdb.h>		
-#include <arpa/inet.h>		
+#include <netdb.h>    
+#include <arpa/inet.h>    
 #include <arpa/telnet.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -21,217 +21,259 @@
 #include "hashtabelle.h"
 
 
-#define HASHSIZE 10
+#define HASHSIZE 3
 
 
 int main(int argc, char** args)
 {
-	
+  
 
 if (args[1]== NULL)
-	{
-		printf("error please type port ") ;
-		exit(1);
-	}
+  {
+    printf("error please type port ") ;
+    exit(1);
+  }
 
 /*
-	****************sockets_create(for local host)*****************
+  ****************sockets_create(for local host)*****************
 */
 
-	int LS ; // listen socket
+  int LS ; // listen socket
 
-	LS = socket(PF_INET,SOCK_STREAM,0);  // creating listen socket 
-	if(LS<0)
-	{
-		printf("cant make server socket") ;
-		exit(1);
-	}
+  LS = socket(PF_INET,SOCK_STREAM,0);  // creating listen socket 
+  if(LS<0)
+  {
+    printf("cant make server socket") ;
+    exit(1);
+  }
 
 
 
 /*
-	****************sockets_intialising(for local host)*****************
+  ****************sockets_intialising(for local host)*****************
 */
-	struct sockaddr_in SA ;
+  struct sockaddr_in SA ;
 
-	bzero(&SA,sizeof(SA)); // putting zeros in it 
+  bzero(&SA,sizeof(SA)); // putting zeros in it 
 
- 	SA.sin_port = htons(atoi(args[1])) ;
-	SA.sin_family = AF_INET ;
-	SA.sin_addr.s_addr = htonl(INADDR_ANY) ; //   local host or INADDR_ANY ***** INet oderr htons  nocmal fragen *****
+  SA.sin_port = htons(atoi(args[1])) ;
+  SA.sin_family = AF_INET ;
+  SA.sin_addr.s_addr = htonl(INADDR_ANY) ; //   local host or INADDR_ANY ***** INet oderr htons  nocmal fragen *****
 
-	if(bind(LS,(struct sockaddr *) &SA,sizeof(SA))<0) // socket, address of host , address length
-	{
-		 printf("cant connect Server bind ") ;
-		 exit(1);
-	}
+  if(bind(LS,(struct sockaddr *) &SA,sizeof(SA))<0) // socket, address of host , address length
+  {
+     printf("cant connect Server bind ") ;
+     exit(1);
+  }
 
 /*
-	****************Connection_Qeue_creating*****************
+  ****************Connection_Qeue_creating*****************
 */
-	if(listen(LS,18) == -1 ) // 18 = how many
-	{ 
-		printf("cant listen Server  ") ;
-		exit(1);
-	}
+  if(listen(LS,18) == -1 ) // 18 = how many
+  { 
+    printf("cant listen Server  ") ;
+    exit(1);
+  }
 
 
 /*
-	****************sockets_intialising(for client address)*****************
+  ****************sockets_intialising(for client address)*****************
 */
-	struct sockaddr_in CA ;
+  struct sockaddr_in CA ;
 
 /*
-	****************Connection_accepting and receiving *****************
-*/		
-	int CS  ;
+  ****************Connection_accepting and receiving *****************
+*/    
+  int CS  ;
 
-	
-	
-	char Buffer [1000000]  ;
-	
-
-
+  
+  
+  char Buffer [10000]  ;
+  
 
 
-	while(1)	
-	{
-		
+  DataItem_i* *hashArray =erstelle(HASHSIZE);
 
-		socklen_t  s = sizeof(CA) ;
-		CS = accept(LS,(struct sockaddr *) &CA,&s);
-		if( CS < 0 ) // accepting
-		{
-			printf("cant  accept  connection Server  ") ;
-			exit(1);
-	    }
+  while(1)  
+  {
+    
 
-
-        DataItem_i* *hashArray =erstelle(HASHSIZE);
-
-		int bytes = recv(CS,Buffer,1000000,0) ; 
-
-		int x = 1 ;
-		int  index4 = 0 ;
-		int  index5 = 0 ;
-		int  index6 = 0 ;
-		int  index7 = 0 ;
-		
-		index4 = Buffer[0]  & (x << 3 ) ;     // get first bite if set to one index4 will be 8
-    	index5 = Buffer[0]  & (x << 2 ) ;  // get 2 bite if set to one index4 will be 4
-		index6 = Buffer[0]  & (x << 1 ) ;  // get 2 bite if set to one index4 will be 2
-		index7 = Buffer[0]  & x   ;		// get 1 bite if set to one index4 will be 2
-		
-		if ((index4+index5+index6+index7)!= 1)
-		{
-			perror("smth went wrong with request Header ");
-		}
-		char tid = Buffer[1];
-
-	   
-        uint16_t   keylen= Buffer[3] + (Buffer[2] <<8 );         // copying the key  len
-        uint16_t   datalen = Buffer[5] + (Buffer[4] <<8 );       // copying the data  len
-
-        bzero(Buffer,1000000) ;
-        int i = bytes ;
-		while(bytes>0)
-		{
-			 bytes = recv(CS,&Buffer[i],1000000,0) ;               
-			 i += bytes;			 
-		}
+    socklen_t  s = sizeof(CA) ;
+    CS = accept(LS,(struct sockaddr *) &CA,&s);
+    if( CS < 0 ) // accepting
+    {
+      printf("cant  accept  connection Server  ") ;
+      exit(1);
+      }
 
 
-        char* key= Buffer +48  ;
-        char* data = key + keylen  ;   
-       
+   
+     bzero(Buffer,10000) ;
+    int bytes = recv(CS,Buffer,10000,0) ; 
+    
+    int x = 1 ;
+    
+    int  index5 = 0 ;   // for get 
+    int  index6 = 0 ;  // for set 
+    int  index7 = 0 ;    // for delete 
+    
+   
+    index5 = Buffer[0]  & (x << 2 ) ;  // get 1 bite if set to one index5 will be 4
+    index6 = Buffer[0]  & (x << 1 ) ;  // get 1 bite if set to one index6 will be 2
+    index7 = Buffer[0]  & x   ;   // get 1 bite if set to one index7 will be 1
+  //  printf("%d\n",index7 );
+   
+    char tid = Buffer[1];
 
-        char answer [1000000]  ;
-        bzero(answer,1000000) ;
-        answer[0]= 8;  // set akn
-       	answer[1]= tid;  // set id
-       	int answersize = 48 ;  // only
-        
+     
+    uint16_t   keylen= Buffer[3] + (Buffer[2] <<8 );         // copying the key  len
+    uint16_t   datalen = Buffer[5] + (Buffer[4] <<8 );       // copying the data  len
 
-        if (index5)
-        {
-        	DataItem_i *requested = get(hashArray,HASHSIZE,key , keylen  );
-        	if( !requested )
-        	     perror("(get )key not found ") 	;
-        	 else{
+   
+    int i = bytes ;
+     
+    while(bytes>0)
+    {
+       bytes = recv(CS,&Buffer[i],10000,0) ;               
+       i += bytes;       
+}
+   // fwrite(Buffer,1,i,stdout) ;
 
-	        	 answer[0]= 12;  // set get and akn
-	        	 
-	        	 answer[2]=  requested->keylen   & 0xFF ;// set last 8 bits to 0
-	        	 answer[3]= (requested->keylen >> 8  & 0xFF);          	 
-	        	 answer[4]=  requested->datalen  & 0xFF;   //set last 8 bits to 0
-	        	 answer[5]= (requested->datalen >> 8)  & 0xFF;  
-	        	 memcpy( &answer[6], requested->key, requested->keylen );
-	        	 memcpy( &answer[6+requested->keylen ], requested->data, requested->datalen );
-	        	 answersize+=  requested->keylen ;
-	        	 answersize+=   requested->datalen ;
-	        	}
+    
+    char* key= NULL ;
+    char* data= NULL ;
+    key = Buffer +6  ;
+     data = key + keylen  ; 
+    char  keydebugguing[10000] ;
+    char  datadebugguing[10000] ;
+    strncpy(keydebugguing,&Buffer [6]  ,keylen); 
+    keydebugguing[6+keylen] = '\0' ;
+    memcpy(datadebugguing,&Buffer[6+keylen ]  ,datalen); 
+    // datadebugguing[5+keylen+datalen] = '\0' ;
+    
+   
+
+    char answer [10000]  ;
+    bzero(answer,10000) ;
+    answer[0]= 8;  // set akn
+
+//TODO change back to tid 
 
 
-        } 
-        else if (index6)
-        {
-        	
-        	if(set(hashArray,HASHSIZE,key,data , keylen ,datalen ) == -1)
-        	     perror(" set hashtabele is full ") 	;
-        	 answer[0]= 10;  // set set and akn
+    answer[1]=tid ;  // set id
+    int answersize = 6 ;  // only
+    
+    
+    if (index5)
+    {
+      DataItem_i *requested = get(hashArray,HASHSIZE,keydebugguing , keylen  );
+       if( !requested )
+       {
+           perror("(get )key not found ")   ;
+       }
+       else
+       {
+         printf("retrived value succefully\n" );
+         answer[0]= 12;  // set get and akn
+         
+         answer[2]=  requested->keylen   & 0xFF ;// set last 8 bits to 0
+         answer[3]= (requested->keylen >> 8  & 0xFF);            
+         answer[4]=  requested->datalen  & 0xFF;   //set last 8 bits to 0
+         answer[5]= (requested->datalen >> 8)  & 0xFF;  
 
-        }      
-        else if (index7)
-        {
-        	if(delete(hashArray,HASHSIZE,key, keylen  ) == -1)
-        	     perror("(delete )could not find the item to delete") 	;
-        	 answer[0]= 9;  // set delete and akn
-
+         strncpy( &answer[6], requested->key, requested->keylen );
+         memcpy( &answer[6+requested->keylen ], requested->data, requested->datalen );
+         answersize+=  requested->keylen ;
+         answersize+=   requested->datalen ;
         }
-       
 
 
-       int bytesent = send(CS,answer,answersize,0);
+    } 
+    else if (index6)
+    {
+      int r = set(hashArray,HASHSIZE,keydebugguing,data , keylen ,datalen ) ;
+      if(r == -1)
+      {
+           perror(" set hashtabele is full ")   ;
+      }
+      else if (r== 0)
+      {
+        printf("added succefully\n");
+      }
+      else if (r == 1)
+      {
+        printf("updated succefully\n");
+      }
+      else if (r == 2)
+      {
+        printf("already inside succefully\n");
+      }
+       answer[0]= 10;  // set set and akn
 
-       if(bytesent )
+     
+
+    }      
+    else if (index7)
+    {
+      if(delete(hashArray,HASHSIZE,keydebugguing, keylen  ) == -1)
+           perror("(delete )could not find the item to delete")   ;
+      else
+        printf("deleted succefully\n");
+       answer[0]= 9;  // set delete and akn
+
+    }
+   
+
+   fwrite(answer,1,answersize,stdout) ;
+   //      printf("%d\n",(int) strlen(answer));
+
+    
+// for (int x = 0; x < answersize; ++x)
+//  {
+//     printf("%d and %c \n",x, answer[x]);
+//  } 
+
+   int bytesent = send(CS,answer,answersize,0);
+
+   if(bytesent !=answersize)
+     perror("error while sending aknowldgment")   ;
 
 
 
 
-   	    
-
- 		
+    
 
 
 
-		
+
+
+    
 
 
 /*
 ***************************start to parse********************
 
 */
-		close(CS);
+    close(CS);
 
-	}		
-	
-
-/*
-	****************recieving*****************
-*/	
-	
-	
-
-
-
+  }   
+  
 
 /*
-	****************closing*****************
+  ****************recieving*****************
+*/  
+  
+  
+
+
+
+
+/*
+  ****************closing*****************
 */
-	
-	// close(LS);
+  
+  // close(LS);
 
 
 }
-
 
